3dc98eaad3d2c53dffd45f060382a00b5cea08f8
Anshul <ans29hul@gmail.com>
2021-05-04 19:50:49 +0530

    init

---
+++ controllers/tweet.js
@@ -0,0 +1,10 @@
+const Tweet = require("../models/Tweet.schema");
+
+//TODO: Implement CRUD Controller
+
+//Create a new Tweet
+exports.create = async (req, res) => {};
+
+//Retrive all Tweets
+exports.findAll = async (req, res) => {};
+
