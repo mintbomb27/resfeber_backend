60a79c19674e3d0c837452614f0efbe0f9189b30
Anshul <ans29hul@gmail.com>
2021-05-04 20:14:19 +0530

    docs: swagger api docs

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,45 @@
 const express = require("express");
-const Tweet = require("../models/Tweet.schema");
-const tweetController = require("../controllers/tweet");
+
+const instaController = require("../controllers/insta");
 const router = express.Router();
 
 module.exports = router;
 
 // TODO: Implement API Routes
 
-router.get("/tweets", tweetController.findAll);
 
+/**
+ * @swagger
+ * /api/get_insta_feed:
+ *  get:
+ *    description: Retrive a list of 10 insta image of a location
+ *    responses:
+ *      '200':
+ *        description: List of 10 url
+ */
+router.get("/get_insta_feed", instaController.get);
+
+
+
+/**
+ * @swagger
+ * /api/tweets/{location}/{resource}:
+ *   get:
+ *     summary: Retrieve a list tweets.
+ *     description: Retrieve a list of tweets based on location and resource type.
+ *     parameters:
+ *             - in: path
+ *               name: location
+ *               type: string
+ *               description: The name of the city to query.
+ *             - in: path
+ *               name: resource
+ *               type: string
+ *               description: The name of the resource to query.
+ *     responses:
+ *       200:
+ *         description: A list of 20 tweets.
+ *         
+ */
+router.get("/something", instaController.get);
 module.exports = router;
