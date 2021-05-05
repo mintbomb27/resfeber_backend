7c29b5b4f89d2eb95bbf314435f943e30773847a
Anshul <ans29hul@gmail.com>
2021-05-05 15:16:37 +0530

    Integrated covid guidelines

--- controllers/oneCallController.js
+++ controllers/oneCallController.js
@@ -2,12 +2,28 @@
 const otmAPI = require('./otma')
 const weatherAPI = require('./weather')
 const instAPI = require('./insta')
+const covidAPI = require('./covid')
+
+
+var NodeGeocoder = require("node-geocoder");
 
 exports.get = async (req,res) => {
     let otm = await otmAPI.otmCall(req.params.location)
     let lat = otm.lat, lon = otm.lon, otmResponse = otm.response
+    
+    var geocoder = NodeGeocoder({
+        provider: "openstreetmap",
+      });
+    
+    let stateData = await geocoder.reverse({ lat: lat, lon: lon })
+    let state = stateData[0].state;
+    console.log("State:",state);
     let weatherResponse = await weatherAPI.weather(req.params.location, lat, lon)
     let instaResponse = await instAPI.get_feed(req.params.location)
-    let data = Object.assign({}, weatherResponse, instaResponse, otmResponse)
+    
+    let covidResponse = await covidAPI.getForState(state)
+    //console.log("Real covid:",covidResponse);
+
+    let data = Object.assign({}, weatherResponse, instaResponse, otmResponse, covidResponse)
     return res.status(200).send(data)
 }
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 
+
+
 //dependencies
 //const cron = require("node-cron");
 const express = require("express");
@@ -8,33 +10,29 @@ const app = express();
 
 //const DB_URL = process.env.DB_URL || "mongodb://localhost:27017/covid_app";
 
-
 const swaggerJsDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 
-  // Swagger config
-  const swaggerOptions = {
-    swaggerDefinition: {
-      info: {
-        version: "1.0.0",
-        title: "Resfeber API",
-        description: "Resfeber API Information",
-        servers: ["http://localhost:4000"]
-      }
+// Swagger config
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      version: "1.0.0",
+      title: "Resfeber API",
+      description: "Resfeber API Information",
+      servers: ["http://localhost:4000"],
     },
-    // ['.routes/*.js']
-    apis: ["routes/apiRoutes.js"]
-  };
+  },
+  // ['.routes/*.js']
+  apis: ["routes/apiRoutes.js"],
+};
 
 //setup Swagger for auto documentation
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-
-
 //Routes
 const apiRoutes = require("./routes/apiRoutes");
-
 
 // mongoose
 //     .connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -47,10 +45,11 @@ app.use(express.json());
 app.use("/api", apiRoutes);
 
 app.use("/", async (req, res) => {
+ 
   res.send("The server is working!");
 });
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-    console.log("🚀 Server Ready!");
+  console.log("🚀 Server Ready!");
 });
